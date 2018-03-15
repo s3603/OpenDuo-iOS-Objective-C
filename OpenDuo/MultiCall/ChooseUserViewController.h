f8849edacdba1b19f4a31b0ba876477e9ccad62f
linyingbin <lin.yb@ihuanyuan.cn>
2018-03-15 17:10:10 +0800

    添加信令sdk

---
+++ OpenDuo/MultiCall/ChooseUserViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChooseUserViewController.h
+//  OpenDuo
+//
+//  Created by 林英彬 on 2018/3/15.
+//  Copyright © 2018年 Agora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChooseUserViewController : UIViewController
+
+@end
